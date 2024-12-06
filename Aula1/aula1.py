93cc254a437345470fe96f6ef5f4526ef7c4b245
Kaynan51 <116405600+Kaynan51@users.noreply.github.com>
2024-12-06 11:12:24 -0300

    Aula 1 com 10 prints

---
+++ Aula1/aula1.py
@@ -0,0 +1,11 @@
+
+print('Bom dia')
+print('Olá Mundo!')
+print('Boa tarde')
+print('Oh Neymar')
+print('Boa noite')
+print('Jair Bolsonaro')
+print('Boa madrugada')
+print('Luiz Inacio Lula da Silva')
+print('Boa tempestade')
+print('Fim da escala 6x1')
